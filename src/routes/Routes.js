04d9b4f7090f17fca5634932f0020902f6decc75
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-05-03 10:34:56 -0300

    feat: adding the components to the routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Routes, Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+import Search from '../pages/Search';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit'
+import NotFound from '../pages/NotFound';
 
 function AppRoutes() {
   return (
     <Routes>
       <Switch>
-        <Route path='/' element='Login' />
-        <Route path='/search' element='Login' />
-        <Route path='/album:id' element='Login' />
-        <Route path='/favorites' element='Login' />
-        <Route path='/profile' element='Login' />
-        <Route path='/profile/edit' element='Login' />
-        <Route path='*' element='NotFound' />
+        <Route path='/' element={<Login />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/album:id' element={<Album />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/edit' element={<ProfileEdit />} />
+        <Route path='*' element={<NotFound />} />
       </Switch>
     </Routes>
   )
