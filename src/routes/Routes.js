2564294ee67227f3fea82ca9b8e6ca578a300adc
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-05-03 10:18:26 -0300

    feat: implementing the routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Routes, Route, Switch } from 'react-router-dom';
 
-function Routes() {
+function AppRoutes() {
   return (
-    <div>Routes</div>
+    <Routes>
+      <Switch>
+        <Route path='/' element='Login' />
+        <Route path='/search' element='Login' />
+        <Route path='/album:id' element='Login' />
+        <Route path='/favorites' element='Login' />
+        <Route path='/profile' element='Login' />
+        <Route path='/profile/edit' element='Login' />
+        <Route path='*' element='NotFound' />
+      </Switch>
+    </Routes>
   )
 }
 
-export default Routes;
+export default AppRoutes;
